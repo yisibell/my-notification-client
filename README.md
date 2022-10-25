1675038d6ad57ec6c1557b303215f4d101655980
elenh <yisiwings@163.com>
2022-10-25 13:52:53 +0800

    feat: notification client socket

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # my-notification-client
-A notification client example.
+
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Notification</h1>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from '@vue/runtime-core'
+import { io } from 'socket.io-client'
+
+const msg = ref()
+
+onMounted(() => {
+  const socket = io('ws://127.0.0.1:3000')
+  socket.on('message', data => {
+    msg.value = data
+  })
+})
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
